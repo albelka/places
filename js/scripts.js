57853620bd82cda590a2370bfa8def54f4ab4211
Anne Belka and Erik Killops <anne@>
2016-10-31 10:48:43 -0700

    add click function to append results to a list and display when individually clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
+//business logic
+function Place (place, landmarks, timeOfYear, notes) {
+  this.place = place;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
 
+//user interface
 $(document).ready(function() {
-  $("#inputs").submit(function(event){
+  $("form#inputs").submit(function(event){
     event.preventDefault();
 
     var place = $("input#place").val();
     var landmarks = $("input#landmarks").val();
     var timeOfYear = $("select#timeOfYear").val();
     var notes = $("input#notes").val();
+    var newPlace =  new Place(place, landmarks, timeOfYear, notes)
 
+    $("ul#places").append("<li><span class='place'>" + newPlace.place + "</span></li>");
 
+    $(".place").click(function(){
+      $("#placeShow").show();
+      $(".placeDisplay").text(newPlace.place);
+      $(".landmarksDisplay").text(newPlace.landmarks);
+      $(".timeOfYearDisplay").text(newPlace.timeOfYear);
+      $(".notesDisplay").text(newPlace.notes);
+    });
+
+    $("input#place").val("");
+    $("input#landmarks").val("");
+    $("select#timeOfYear").val("");
+    $("input#notes").val("");
   });
 });
